356327f54d9edb8662da3901bcfc609f87995af8
Tatvesh Sawant <92212661+TatveshS@users.noreply.github.com>
2021-12-24 22:00:42 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home({propertiesForSale , propertiesForRent}) {
         buttonText='Explore Renting'
         linkName='/search?purpose=for-rent'
         imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
-        buttonText='Rent'
+    
       
       />
       <Flex flexWrap='wrap'>
