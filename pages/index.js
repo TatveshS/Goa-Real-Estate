7f1823b3ce850500c2d9477b49f31023dc10291b
Tatvesh Sawant <92212661+TatveshS@users.noreply.github.com>
2021-12-24 22:01:10 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ export default function Home({propertiesForSale , propertiesForRent}) {
          buttonText='Explore Buying'
          linkName='/search?purpose=for-sale'
          imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
-         buttonText='Buy'
+       
       
       />
       <Flex flexWrap='wrap' >      {propertiesForSale.map((property) => <Property property={property} key={property.id} />)}</Flex>
